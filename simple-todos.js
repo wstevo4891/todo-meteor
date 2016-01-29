b4e89b072e7a6baa81fe5089ab0fc9ec89e2a47c
Eric Stephenson <ericb.stephenson@gmail.com>
2016-01-29 14:27:31 -0800

    first commit

---
+++ simple-todos.js
@@ -0,0 +1,9 @@
+if (Meteor.isClient) {
+  Template.body.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
+  });
+}
