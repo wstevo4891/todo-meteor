6f16e4947b921e1c2aba5404b5e2376a5b2af9ea
Eric Stephenson <ericb.stephenson@gmail.com>
2016-01-29 15:14:50 -0800

    added checkboxes and delete buttons

--- simple-todos.js
+++ simple-todos.js
@@ -25,4 +25,37 @@ if (Meteor.isClient) {
       event.target.text.value = "";
     }
   });
+
+  Template.tasks.events({
+    "click .toggle-checked": function() {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
+  });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
