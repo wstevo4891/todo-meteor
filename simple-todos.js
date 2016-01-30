6b3fda0d85d08c976a0d1f6f971e4a67f295e15e
Eric Stephenson <ericb.stephenson@gmail.com>
2016-01-30 09:59:30 -0800

    removed autopublish, added publish and subscribe

--- simple-todos.js
+++ simple-todos.js
@@ -1,6 +1,20 @@
 Tasks = new Mongo.Collection("tasks");
 
+if (Meteor.isServer) {
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish("tasks", function() {
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
+  });
+}
+
 if (Meteor.isClient) {
+  Meteor.subscribe("tasks");
+
   Template.body.helpers({
     tasks: function() {
       if (Session.get("hideCompleted")) {
@@ -38,6 +52,12 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.task.helpers({
+    isOwner: function() {
+      return this.owner === Meteor.userId();
+    }
+  });
+
   Template.task.events({
     "click .toggle-checked": function() {
       // Set the checked property to the opposite of its current value
@@ -45,6 +65,9 @@ if (Meteor.isClient) {
     },
     "click .delete": function() {
       Meteor.call("deleteTask", this._id);
+    },
+    "click .toggle-private": function() {
+      Meteor.call("setPrivate", this._id, ! this.private);
     }
   });
 
@@ -68,10 +91,32 @@ Meteor.methods({
     });
   },
   deleteTask: function(taskId) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
+
     Tasks.remove(taskId);
   },
   setChecked: function(taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error("not-authorized");
+    }
+    
     Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+  setPrivate: function(taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+
+    //Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   }
 });
 
